2c79d060aefd329929878dfa923439dd67bae311
Jeremy <jeremybusuttil5@gmail.com>
2020-12-15 16:11:11 +0100

    Added a separate Activity for the recommended Items. Created a layout for a single row in the list view named item_row.xml.

---
+++ app/src/main/java/com/example/intelligentinterfaces2/RecommenderActivity.java
@@ -0,0 +1,14 @@
+package com.example.intelligentinterfaces2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecommenderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommender);
+    }
+}
