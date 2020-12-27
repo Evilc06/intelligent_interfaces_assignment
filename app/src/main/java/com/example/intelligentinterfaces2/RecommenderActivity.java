b3509e0f2bc425f45b65f8fd137b071d7337dfb9
Emma Harrison <emma.harrison.18@um.edu.mt>
2020-12-27 19:32:06 +0100

    Added recommended shops functionality with UI

--- app/src/main/java/com/example/intelligentinterfaces2/RecommenderActivity.java
+++ app/src/main/java/com/example/intelligentinterfaces2/RecommenderActivity.java
@@ -1,22 +1,12 @@
 package com.example.intelligentinterfaces2;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
@@ -47,6 +37,4 @@ public class RecommenderActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
